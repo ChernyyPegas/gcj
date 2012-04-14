82affa4856aee1031c2347498c594ada9c52db18
Hash <takuya21hashimoto@gmail.com>
2012-04-15 04:00:12 +0900

    Problem A. Speaking in Tongues

---
+++ jamA.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+MAPPING = {
+ "a" => "y",
+ "b" => "h",
+ "c" => "e",
+ "d" => "s",
+ "e" => "o",
+ "f" => "c",
+ "g" => "v",
+ "h" => "x",
+ "i" => "d",
+ "j" => "u",
+ "k" => "i",
+ "l" => "g",
+ "m" => "l",
+ "n" => "b",
+ "o" => "k",
+ "p" => "r",
+ "q" => "z",
+ "r" => "t",
+ "s" => "n",
+ "t" => "w",
+ "u" => "j",
+ "v" => "p",
+ "w" => "f",
+ "x" => "m",
+ "y" => "a",
+ "z" => "q",
+ " " => " "
+}
+
+#puts  MAPPING.keys.map{|k| MAPPING.values.include?(k)}
+
+def mapping(str)
+  str.split("").map{|s| MAPPING[s] }.join
+end
+
+@cases = []
+File.open("/Users/hash/Downloads/A-small-attempt1.in"){|file|
+  row = 0
+  while line = file.gets
+    row = row + 1
+    next if row == 1
+    @cases << line.chomp!
+  end
+}
+
+
+@cases.each_with_index do |c, i|
+  answer = mapping(c)
+  puts "Case ##{i+1}: " + answer
+end
+
